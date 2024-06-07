a4e4e42bb8462eca51763d8ef21a04d57c2abd23
3xiic <122654327+3xiic@users.noreply.github.com>
2024-06-06 23:55:18 -0500

    Se agrega

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { FondoComponent } from './fondo/fondo.component';
+import { ServiciosComponent } from './servicios/servicios.component';
+import { RequisitosComponent } from './requisitos/requisitos.component';
+import { ExperienciasComponent } from './experiencias/experiencias.component';
+import { BlogComponent } from './blog/blog.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    FondoComponent,
+    ServiciosComponent,
+    RequisitosComponent,
+    ExperienciasComponent,
+    BlogComponent
   ],
   imports: [
     BrowserModule,
