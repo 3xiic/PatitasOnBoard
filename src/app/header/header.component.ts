4bf910a9a24af466a9389212839d6bee966c6ed9
3xiic <122654327+3xiic@users.noreply.github.com>
2024-06-20 03:22:43 -0500

    Act Servicios, Carousel Exp, NavBar

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -11,17 +11,20 @@ export class HeaderComponent {
 
   public scrollSubscription!: Subscription;
   public isScrolled: boolean = false;
+  public isScrolledPink: boolean = false;
+  public isScrolledPurple: boolean = false;
 
   constructor(private scrollService: ScrollService){}
 
   ngOnInit(): void {
-    
     this.scrollSubscription = this.scrollService.getScroll().subscribe(scrollPosition => {
       this.isScrolled = scrollPosition > 0;
-    })
-
-    
-
+      this.isScrolledPink = scrollPosition > 1800; 
+      if(this.isScrolledPink && scrollPosition > 3900){
+        this.isScrolledPink = false;
+      }
+      
+    });
   }
 
 }
